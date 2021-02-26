86345525a557995ffa90a004507113f6b5522d48
Rodrigo Tuna <up201904967@fe.up.pt>
2021-02-26 23:24:33 +0000

    Fixed coordinates

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -56,7 +56,7 @@ export class MyTangram extends CGFobject {
           this.scene.popMatrix();
       
           this.scene.pushMatrix();
-          this.scene.translate(0.0,Math.sqrt(2.0)-0.8,0.0);
+          this.scene.translate(0.0,2.0 - Math.sqrt(2.0),0.0);
           this.scene.rotate(Math.PI/2,0.0,0.0,1.0);
           this.scene.setOrangeAppearance();
           this.triangleBig.display();
