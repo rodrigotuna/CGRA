aef3e4ed888ee43e31dba9646dddd0a1f0b05069
Rodrigo Tuna <up201904967@fe.up.pt>
2021-03-03 17:09:57 +0000

    first set of normals

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -2,6 +2,9 @@ import { CGFscene, CGFcamera, CGFaxis, CGFappearance } from "../lib/CGF.js";
 import { MyPyramid } from "./MyPyramid.js";
 import { MyCone } from "./MyCone.js";
 import { MyPlane } from "./MyPlane.js";
+import { MyTangram } from "./MyTangram.js";
+import { MyUnitCube } from "./MyUnitCube.js";
+import { MyDiamond } from "./MyDiamond.js";
 
 /**
 * MyScene
@@ -30,11 +33,14 @@ export class MyScene extends CGFscene {
         this.plane = new MyPlane(this, 5);
         this.cone = new MyCone(this, 3, 1);
         this.pyramid = new MyPyramid(this, 3, 1);
+        this.tangram = new MyTangram(this);
+        this.unitCube = new MyUnitCube(this);
+        this.diamond = new MyDiamond(this);
         
-        this.objects = [this.plane, this.pyramid, this.cone];
+        this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.unitCube, this.diamond];
 
         // Labels and ID's for object selection on MyInterface
-        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2};
+        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram': 3, 'Unit Cube': 4, 'Diamond': 5};
 
         //Other variables connected to MyInterface
         this.selectedObject = 0;
@@ -43,6 +49,7 @@ export class MyScene extends CGFscene {
         this.displayNormals = false;
         this.objectComplexity = 0.5;
         this.scaleFactor = 2.0;
+        this.ambientIntensity = 0.3;
 
     }
     initLights() {
@@ -155,6 +162,8 @@ export class MyScene extends CGFscene {
         
         this.lights[0].update();
         this.lights[1].update();
+
+        this.setGlobalAmbientLight(this.ambientIntensity, this.ambientIntensity, this.ambientIntensity, 1.0);
 
         // Draw axis
         if (this.displayAxis)
