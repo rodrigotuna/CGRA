f9b727fdafb9fa74aa2f6f560de136e596633635
“Maria <up201907726@fe.up.pt>
2021-03-10 12:08:51 +0000

    Tangram Base Code

---
+++ tp4/MyParallelogram.js
@@ -0,0 +1,47 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyParallelogram
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	
+	initBuffers() {
+		this.vertices = [
+			0, 0, 0,	//0
+			2, 0, 0,	//1
+            3, 1, 0,	//2
+			1, 1, 0,    //3
+			0, 0, 0,	
+			2, 0, 0,
+            3, 1, 0,
+            1, 1, 0
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+            0, 1, 2,
+            2, 3, 0,
+			6, 5, 4,
+            4, 7, 6,
+		];
+
+		this.normals = [];
+		for(var i = 0; i < 4; i++){
+			this.normals.push(0,0,-1);
+		}
+		for(var i = 0; i < 4; i++){
+			this.normals.push(0,0,1);
+		}
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
