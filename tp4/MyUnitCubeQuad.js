cdd5aa28326e5be8ce33a5f0b350628dd74b34d0
Rodrigo Tuna <up201904967@fe.up.pt>
2021-03-10 18:32:35 +0000

    improve part2

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -52,12 +52,12 @@ export class MyUnitCubeQuad extends CGFobject {
         this.material.setTexture(this.textureList[2]);
         this.material.apply();
         this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.quad.updateTexCoords([
+        /*this.quad.updateTexCoords([
             1, 1,
             0, 1,
             1, 0,
             0, 0
-        ])
+        ])*/
         this.scene.pushMatrix();
         this.scene.translate(0,0,0.5);
         this.quad.display();
@@ -66,27 +66,27 @@ export class MyUnitCubeQuad extends CGFobject {
         this.material.setTexture(this.textureList[3]);
         this.material.apply();
         this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.quad.updateTexCoords([
+        /*this.quad.updateTexCoords([
             1, 0,
             0, 0,
             1, 1,
             0, 1
-        ])
+        ])*/
         this.scene.pushMatrix();
         this.scene.translate(0,0,-0.5);
-        this.scene.rotate(Math.PI,1,0,0);
+        this.scene.rotate(Math.PI,0,1,0);
         this.quad.display();
         this.scene.popMatrix();
 
         this.material.setTexture(this.textureList[4]);
         this.material.apply();
         this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.quad.updateTexCoords([
+        /*this.quad.updateTexCoords([
             0, 1,
             1, 1,
             0, 0,
             1, 0
-        ])
+        ])*/
         this.scene.pushMatrix();
         this.scene.translate(0.5,0,0);
         this.scene.rotate(Math.PI/2,0,1,0);
@@ -96,12 +96,12 @@ export class MyUnitCubeQuad extends CGFobject {
         this.material.setTexture(this.textureList[5]);
         this.material.apply();
         this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        this.quad.updateTexCoords([
+        /*this.quad.updateTexCoords([
             1, 1,
             0, 1,
             1, 0,
             0, 0
-        ])
+        ])*/
         this.scene.pushMatrix();
         this.scene.translate(-0.5,0,0);
         this.scene.rotate(-Math.PI/2,0,1,0);
