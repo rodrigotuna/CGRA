3133d0a7f7516235ed9b4fb1be00c995e54ede57
Rodrigo Tuna <up201904967@fe.up.pt>
2021-04-28 11:50:25 +0100

    added normals to fish

--- project/MyTriangle.js
+++ project/MyTriangle.js
@@ -15,12 +15,24 @@ export class MyTriangle extends CGFobject {
 			0, 2, 0,	//0
 			0, 0, 0,	//1
 			2, 0, 0,	//2
+			0, 2, 0,
+			0, 0, 0,
+			2, 0, 0
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
-			2, 1 ,0
+			5, 4 ,3
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1
 		];
 
 		//The defined indices (and corresponding vertices)
